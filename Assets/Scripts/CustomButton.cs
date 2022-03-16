2746c7145393405b2a86a58c2027d7ed3b03edc6
Jakub Nowak <jakub_nowak@interia.pl>
2022-03-16 22:30:06 +0100

    Started custom buttons bhv

---
+++ Assets/Scripts/CustomButton.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CustomButton : MonoBehaviour
+{
+    public CanvasGroup bgdAlpha;
+
+    public float fadeSpeed;
+
+    private void Start()
+    {
+        bgdAlpha = gameObject.GetComponent<CanvasGroup>();
+    }
+
+    public void FadeIn()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeInIE());
+    }
+
+    public void FadeOut()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeOutIE());
+    }
+
+    IEnumerator FadeInIE()
+    {
+        bgdAlpha.alpha = Mathf.Lerp(bgdAlpha.alpha, 1, fadeSpeed * Time.deltaTime);
+
+        if(bgdAlpha.alpha >= 0.95f)
+        {
+            bgdAlpha.alpha = 1;
+            yield return new WaitForEndOfFrame();
+        }
+        else
+        {
+            yield return new WaitForEndOfFrame();
+            StartCoroutine(FadeInIE());
+        }
+    }
+
+    //nie dziala
+    IEnumerator FadeOutIE()
+    {
+        bgdAlpha.alpha = Mathf.Lerp(bgdAlpha.alpha, 0, fadeSpeed * Time.deltaTime);
+
+        if (bgdAlpha.alpha <= 0.05f)
+        {
+            bgdAlpha.alpha = 0;
+            yield return new WaitForEndOfFrame();
+        }
+        else
+        {
+            yield return new WaitForEndOfFrame();
+            StartCoroutine(FadeInIE());
+        }
+    }
+}
